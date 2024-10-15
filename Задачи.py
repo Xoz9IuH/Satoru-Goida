141496790ad9982f9238137cc72eebf03c9a7894
Xoz9IuH <olegbundovic@gmail.com>
2024-10-15 13:07:45 +0300

    Сатору гойда

--- Задачи.py
+++ Задачи.py
@@ -1,22 +1,22 @@
-# abc = int(123) 
-# a = abc % 10
-# b = abc % 100
-# b = b // 10
-# abc = abc // 100
-# print (str(abc) + str(a) + str(b))
+abc = int(123) 
+a = abc % 10
+b = abc % 100
+b = b // 10
+abc = abc // 100
+print (str(abc) + str(a) + str(b))
 
 
-# a = 1234
-# a = (a // 100) % 10
-# print (a)
+a = 1234
+a = (a // 100) % 10
+print (a)
 
 
-# a = 101
-# if a == 0: print("Нулевое число")
-# elif a < 0 and a% 2 == 0: print ("отрицательное четное число")
-# elif a > 0 and a% 2 == 0: print ("положительное четное число")
-# elif a < 0 and a % 2 != 0: print ("отрицательное не четное число")
-# elif a > 0 and a% 2 != 0: print ("положительно не четное число")
+a = 101
+if a == 0: print("Нулевое число")
+elif a < 0 and a% 2 == 0: print ("отрицательное четное число")
+elif a > 0 and a% 2 == 0: print ("положительное четное число")
+elif a < 0 and a % 2 != 0: print ("отрицательное не четное число")
+elif a > 0 and a% 2 != 0: print ("положительно не четное число")
 
 
 a = int(2)
